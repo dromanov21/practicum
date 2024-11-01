82cde82e630bdb34f9957a7f6fcc5361fe9ba2e5
dromanov21 <57029434+dromanov21@users.noreply.github.com>
2024-11-01 14:36:34 +0300

    Update 6.40.c

--- 6.40.c
+++ 6.40.c
@@ -5,7 +5,7 @@ struct node{
     char data;
     struct node *next;
 };
-int occurrence_element(struct node *head, char elem)
+int occur_elem(struct node *head, char elem)
 {
     int a = 0;
     struct node *tmp = head;
@@ -18,14 +18,14 @@ int occurrence_element(struct node *head, char elem)
     return a;
 }
 
-void make_new_list(struct node *head1, struct node *head2, struct node *head3){
+void make_list(struct node *head1, struct node *head2, struct node *head3){
     struct node *tmp1 = head1;
     struct node *tmp2 = head2;
     struct node *tmp3 = head3;
     
     while (head1->next != NULL)
     {
-        if (occurrence_element(head2, head1->data) == 0)
+        if (occur_elem(head2, head1->data) == 0)
         {
             head3->data = head1->data;
             head3->next = (struct node *)malloc(sizeof(struct node));
@@ -45,7 +45,7 @@ int main(void){
     struct node *head1 = (struct node *)malloc(sizeof(struct node));
     struct node *tmp1 = head1;
     
-    printf("Enter elements of the first list: ");
+    printf("Enter elements to first list: ");
     scanf("%c", &(head1->data));
     while (head1->data != '.'){
         head1->next = (struct node *)malloc(sizeof(struct node));
@@ -58,7 +58,7 @@ int main(void){
     struct node *head2 = (struct node *)malloc(sizeof(struct node));
     struct node *tmp2 = head2;
     
-    printf("Enter elements of the second list: ");
+    printf("Enter elements to second list: ");
     scanf("%c", &(head2->data));
     while (head2->data != '.'){
         head2->next = (struct node *)malloc(sizeof(struct node));
@@ -86,7 +86,7 @@ int main(void){
     struct node *head3 = (struct node *)malloc(sizeof(struct node));
     struct node *tmp3 = head3;
     
-    make_new_list(head1, head2, head3);
+    make_list(head1, head2, head3);
     printf("\n");
     printf("third list: \n");
     while (head3->data != '.'){
