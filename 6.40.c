6a537e2ed5aac5a03c6b4ecc3f7cd39b46131321
dromanov21 <57029434+dromanov21@users.noreply.github.com>
2024-11-22 02:02:43 +0300

    Update 6.40.c

--- 6.40.c
+++ 6.40.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct node{
-    char data;
+struct node
+{
+    char *data;
     struct node *next;
 };
-int occur_elem(struct node *head, char elem)
+int add(struct node *head, char *elem)
 {
     int a = 0;
     struct node *tmp = head;
-    while (head->data != '.')
+    while (head->data != ".")
     {
         if (head->data == elem) a = 1;
         head = head->next;
@@ -18,14 +19,14 @@ int occur_elem(struct node *head, char elem)
     return a;
 }
 
-void make_list(struct node *head1, struct node *head2, struct node *head3){
+void makel(struct node *head1, struct node *head2, struct node *head3){
     struct node *tmp1 = head1;
     struct node *tmp2 = head2;
     struct node *tmp3 = head3;
     
-    while (head1->next != NULL)
+    while (head1->data != ".")
     {
-        if (occur_elem(head2, head1->data) == 0)
+        if (add(head2, head1->data) == 0)
         {
             head3->data = head1->data;
             head3->next = (struct node *)malloc(sizeof(struct node));
@@ -34,10 +35,10 @@ void make_list(struct node *head1, struct node *head2, struct node *head3){
         head1 = head1->next;
     }
     head1 = tmp1;
-
-    while (head2->next != NULL)
+    head2 = tmp2;
+    while (head2->data != ".")
     {
-        if (occur_elem(head1, head2->data) == 0)
+        if (add(head1, head2->data) == 0)
         {
             head3->data = head2->data;
             head3->next = (struct node *)malloc(sizeof(struct node));
@@ -46,65 +47,70 @@ void make_list(struct node *head1, struct node *head2, struct node *head3){
         head2 = head2->next;
     }
     head3->next = (struct node *)malloc(sizeof(struct node));
-    head3->data = '.';
+    head3->data = ".";
     head3->next = NULL;
     head1 = tmp1;
     head2 = tmp2;
     head3 = tmp3;
 }
 
+void printl(struct node *head)
+{
+    struct node *tmp = head;
+    while (head->data != "."){
+        printf("%s\n", head->data);
+        head= head->next;
+    }
+    head = tmp;
+}
+
 int main(void){
     struct node *head1 = (struct node *)malloc(sizeof(struct node));
     struct node *tmp1 = head1;
     
-    printf("Enter elements to first list: ");
-    scanf("%c", &(head1->data));
-    while (head1->data != '.'){
-        head1->next = (struct node *)malloc(sizeof(struct node));
-        head1 = head1->next;
-        scanf("%c", &head1->data);
-    }
+    printf("first list: \n"); 
+    head1->data = "one";
+    head1->next = (struct node *)malloc(sizeof(struct node));
+    head1 = head1->next;
+    head1->data = "two";
+    head1->next = (struct node *)malloc(sizeof(struct node));
+    head1 = head1->next;  
+    head1->data = "three";
+    head1->next = (struct node *)malloc(sizeof(struct node));
+    head1 = head1->next;   
+    head1->data = "four";
+    head1->next = (struct node *)malloc(sizeof(struct node));
+    head1 = head1->next;  
+    head1->data = ".";
     head1->next = NULL;
     head1 = tmp1;
+    printl(head1);
     
+    printf("\nsecond list: \n");
     struct node *head2 = (struct node *)malloc(sizeof(struct node));
     struct node *tmp2 = head2;
-    
-    printf("Enter elements to second list: ");
-    scanf("%c", &(head2->data));
-    while (head2->data != '.'){
-        head2->next = (struct node *)malloc(sizeof(struct node));
-        head2 = head2->next;
-        scanf("%c", &head2->data);
-    }
+    head2->data = "three";
+    head2->next = (struct node *)malloc(sizeof(struct node));
+    head2 = head2->next;   
+    head2->data = "four";
+    head2->next = (struct node *)malloc(sizeof(struct node));
+    head2 = head2->next; 
+    head2->data = "five";
+    head2->next = (struct node *)malloc(sizeof(struct node));
+    head2 = head2->next;
+    head2->data = "six";
+    head2->next = (struct node *)malloc(sizeof(struct node));
+    head2 = head2->next;
+    head2->data = ".";
     head2->next = NULL;
     head2 = tmp2;
+    printl(head2);
     
-    printf("---------------------\n");
-    printf("first list: \n");
-    while (head1->data != '.'){
-        printf("%c", head1->data);
-        head1 = head1->next;
-    }
-    head1 = tmp1;
-    
-    printf("\nsecond list: ");
-    while (head2->data != '.'){
-        printf("%c", head2->data);
-        head2 = head2->next;
-    }
-    head2 = tmp2;
-    
+    printf("\nthird list: \n");
     struct node *head3 = (struct node *)malloc(sizeof(struct node));
     struct node *tmp3 = head3;
+    makel(head1, head2, head3);
+    printl(head3);
     
-    make_list(head1, head2, head3);
-    printf("\n");
-    printf("third list: \n");
-    while (head3->data != '.'){
-        printf("%c\n", head3->data);
-        head3 = head3->next;
-    }
-    head3 = tmp3;
     return 0;
 }
